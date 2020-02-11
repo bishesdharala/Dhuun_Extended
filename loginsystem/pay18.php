205f348a985d4cf77a17e0a45e5d80be55540131
OperatorParasite <30445334+bishesdharala@users.noreply.github.com>
2020-02-11 21:01:17 +0545

    Add files via upload

---
+++ loginsystem/pay18.php
@@ -0,0 +1,104 @@
+<?php session_start();
+require_once('dbconnection.php');
+
+// Code for login 
+if(isset($_POST['login']))
+{
+$password=$_POST['password'];
+$dec_password=$password;
+$useremail=$_POST['uemail'];
+$ret= mysqli_query($con,"SELECT * FROM users WHERE email='$useremail' and password='$dec_password'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+$extra="paydone18.php";
+$_SESSION['login']=$_POST['uemail'];
+$_SESSION['id']=$num['id'];
+$_SESSION['name']=$num['fname'];
+$host=$_SERVER['HTTP_HOST'];
+$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+else
+{
+echo "<script>alert('Invalid username or password');</script>";
+$extra="index.php";
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+//header("location:http://$host$uri/$extra");
+exit();
+}
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Advanced Payment System</title>
+<link href="css/pay18.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Elegent Tab Forms,Login Forms,Sign up Forms,Registration Forms,News latter Forms,Elements"./>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+</script>
+<script src="js/jquery.min.js"></script>
+<script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+				<script type="text/javascript">
+					$(document).ready(function () {
+						$('#horizontalTab').easyResponsiveTabs({
+							type: 'default',       
+							width: 'auto', 
+							fit: true 
+						});
+					});
+				   </script>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,600,700,200italic,300italic,400italic,600italic|Lora:400,700,400italic,700italic|Raleway:400,500,300,600,700,200,100' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="main">
+		<h1>Advanced Payment System</h1>
+	 <div class="sap_tabs">	
+			<div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
+			  <ul class="resp-tabs-list">			  	  	
+				</li>
+				  <li class="resp-tab-item" aria-controls="tab_item-1" role="tab"><div class="top-img"><img src="images/top-lock.png" alt=""/></div><span>Your Total = 18$</span></li>
+				  <div class="clear"></div>
+			  </ul>		
+			  	 
+			<div class="resp-tabs-container">
+			 <div class="tab-2 resp-tab-content" aria-labelledby="tab_item-1">
+					 	<div class="facts">
+							 <div class="login">
+							<div class="buttons">
+								
+								
+							</div>
+							<form name="login" action="" method="post">
+								<input type="text" class="text" name="uemail" value="" placeholder="Enter your email"  ><a href="#" class=" icon email"></a>
+
+                                <input type="password" value="" name="password" placeholder="Enter your password"><a href="#" class=" icon lock"></a>
+                                <input type="text" class="text" name="cardno" value="" placeholder="Enter your card number"  ><a href="#" class=" icon email"></a>
+                                <input type="text" class="text" name="expiry" value="" placeholder="Expiry date d/m/y"  ><a href="#" class=" icon email"></a>
+                                <input type="text" class="text" name="CVV" value="" placeholder="Enter your CVV"  ><a href="#" class=" icon email"></a>
+                                <input type="text" class="text" name="address" value="" placeholder="Your address"  ><a href="#" class=" icon email"></a>
+								<div class="p-container">
+								
+									<div class="submit two">
+									<input type="submit" name="login" value="Proceed to payment" >
+									</div>
+									<div class="clear"> </div>
+								</div>
+
+							</form>
+					</div>
+				</div> 
+			</div> 			        					 
+				 
+				     </div>	
+		        </div>
+	        </div>
+	     </div>
+
+</body>
+</html>
